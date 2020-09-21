d5f2a2316759de76d7c9c362b7fd53c7d482d3fb
Mario Fernandez <diddleslip@gmail.com>
2020-09-21 12:34:11 -0400

    Re-located components to seperate folders

--- filter-data/src/App.js
+++ filter-data/src/App.js
@@ -1,48 +1,13 @@
-import React, { useState } from 'react';
-import {filterData} from "./components/FilterableData";
-import * as FaIcons from 'react-icons/fa';
-import * as AiIcons from 'react-icons/ai';
-import * as IoIcons from 'react-icons/io';
+import React from 'react';
+import Filter from "./components/Filter";
 
 import './App.css';
 
 function App() {
-  const [data, setData] = useState(filterData);
-  const [number, setNumber ] = useState(1);
 
-  const filterA = () => {
-    setData([...data.sort((a,b) => {
-      return a.localeCompare(b);
-    })])
-  }
-
-  const filterB = () => {
-    setData([...data.sort((a,b) => {
-      return b.localeCompare(a);
-    })])
-  }
-
-  // console.log("This is data: ", data)
   return (
     <div className="App">
-
-
-      <h1>Names: </h1>
-      <span onClick={() => {
-          number === 1 ? filterA() :filterB()
-          
-          // Changes between value 1-2
-          number === 1 ? setNumber(2) : setNumber(1)
-        }}
-      >
-        A-Z <FaIcons.FaSort />
-      </span>
-      {data.map((name, index) => {
-
-        return(
-          <h3 key={index}>{name}</h3>
-        )
-      })}
+      <Filter/>
     </div>
   );
 }
--- filter-data/src/components/FilterableData.js
+++ filter-data/src/components/FilterableData.js
@@ -10,3 +10,23 @@ export const filterData = [
 	"Mason",
 	"Ethan",
 ];
+
+export const filterData2= [
+  {
+    "id": "RecordId1",
+    "name": "Bradford Soap Works",
+    "type": {
+      "name": "Manufacturing Partners"
+    },
+    "coordinates": {
+      "latitude": 41.715078,
+      "longitude": -71.510541
+    },
+    "fields": [
+      {
+        "name": "Website",
+        "value": "http://www.bradfordsoap.com/"
+      }
+    ]
+  }
+]
